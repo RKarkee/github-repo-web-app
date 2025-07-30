a582deeb29169d638010597401ca06ed8f632eab
RKarkee <rajankarkee01@gmail.com>
2025-07-30 16:23:53 +0545

    react query added  for state management

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px", // Custom small screen size
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1536px",
+      },
+    },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
