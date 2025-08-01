91b80f08108982f04c15cc5fa941eda83613d785
RKarkee <rajankarkee01@gmail.com>
2025-08-01 23:45:00 +0545

    github api for production added.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
-export const GITHUB_API_BASE = import.meta.env.VITE_GITHUB_API_BASE_URL ;
+export const GITHUB_API_BASE =
+  import.meta.env.VITE_GITHUB_API_BASE_URL || "https://api.github.com";
 export const DEFAULT_PER_PAGE = 25;
 export const DEFAULT_SORT = "stars";
 export const DEFAULT_ORDER = "desc";
